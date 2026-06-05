5627870ad8fb3d605326bbd4d69b26474803acb4
Swayam Singhai <swayamsinghai212@gmail.com>
2026-06-05 15:05:15 +0530

    feat: setup backend and connect mongodb atlas

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("ST Carpool Backend Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
