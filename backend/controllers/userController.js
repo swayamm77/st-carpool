fcd67d619435b614bf0e0b0468731f3fc1c1c25a
Swayam Singhai <swayamsinghai212@gmail.com>
2026-06-09 13:30:03 +0530

    Add user lookup endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,7 +24,28 @@ const getUsers = async (req, res) => {
   }
 };
 
+const getUserByEmail = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      email: req.params.email,
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   createUser,
   getUsers,
+  getUserByEmail,
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
   createUser,
   getUsers,
+  getUserByEmail,
 } = require("../controllers/userController");
 
 router.post("/", createUser);
 router.get("/", getUsers);
+router.get("/email/:email", getUserByEmail);
 
 module.exports = router;
