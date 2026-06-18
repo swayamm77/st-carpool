5b3d726edb376554664adb910757b814442a80e8
Swayam Singhai <swayamsinghai212@gmail.com>
2026-06-18 10:16:26 +0530

    fixed errors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+
 const userRoutes = require("./routes/userRoutes");
 const rideRoutes = require("./routes/rideRoutes");
 const requestRoutes = require("./routes/requestRoutes");
@@ -12,31 +13,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use("/api/users", userRoutes);
-app.use("/api/rides", rideRoutes);
-app.use("/api/requests", requestRoutes);
 
-app.get("/", (req, res) => {
-  res.send("ST Carpool Backend Running");
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  const express = require("express");
-const dotenv = require("dotenv");
-const connectDB = require("./config/db");
-const userRoutes = require("./routes/userRoutes");
-const rideRoutes = require("./routes/rideRoutes");
-const requestRoutes = require("./routes/requestRoutes");
-
-dotenv.config();
-
-connectDB();
-
-const app = express();
-
-app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/rides", rideRoutes);
 app.use("/api/requests", requestRoutes);
